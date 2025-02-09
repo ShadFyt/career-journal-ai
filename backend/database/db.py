8a23c28265c3bff0ab03af8b4ae8e0edc6b179da
ShadFyt <doanryan10@gmail.com>
2025-02-09 10:42:09 -0500

    remove unused imports

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,8 +1,6 @@
-from typing import Annotated
-from fastapi import Depends
-from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel import SQLModel, create_engine
 
-sqlite_file_name = "backend/database.db"
+sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
@@ -10,11 +8,3 @@ engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-
-SessionDep = Annotated[Session, Depends(get_session)]
