9395c3d2089a8efd50a6d9b67049fd63882619e8
ShadFyt <doanryan10@gmail.com>
2025-02-25 22:45:33 -0500

    feat(user): create user service dependency

---
+++ backend/domain/user/user_dependencies.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+
+from database.session import SessionDep
+from domain.user.user_repo import UserRepo
+from domain.user.user_service import UserService
+from fastapi import Depends
+
+
+def get_user_repo(session: SessionDep) -> UserRepo:
+    return UserRepo(session=session)
+
+
+def get_user_service(user_repo: UserRepo) -> UserService:
+    return UserService(user_repo=user_repo)
+
+
+UserServiceDep = Annotated[UserService, Depends(get_user_service)]
