96c4a0cd78593909ea1a0b04378da0e35c6770b0
ShadFyt <doanryan10@gmail.com>
2025-02-28 23:26:17 -0500

    refactor(routing): migrate to file-based routing
    - Rename views to pages directory
    - Convert HomeView.vue to index.vue
    - Convert NotFoundView.vue to [...catchAll].vue
    - Update router configuration to use file-based routing

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,20 +1,9 @@
-import HomeView from '@/views/HomeView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: HomeView,
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'NotFound',
-      component: () => import('@/views/NotFoundView.vue'),
-    },
-  ],
+  routes
 })
 
 export default router
