afcd7bebe3fb7211364cce7d50ca9a4eff618945
ShadFyt <doanryan10@gmail.com>
2025-02-28 23:14:11 -0500

    feat(routing): setup VueRouter plugin for file based routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwind from 'tailwindcss'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    VueRouter(),
     vue(),
     vueDevTools(),
   ],
