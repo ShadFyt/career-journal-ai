e333dc9e0dd5f5f8ff4904d14ac3d19828f3ff12
ShadFyt <doanryan10@gmail.com>
2025-03-01 23:08:24 -0500

    feat(routing): setup async routing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component, route }">
+    <Suspense v-if="Component">
+      <Component :is="Component" :key="route.name" />
+    </Suspense>
+  </RouterView>
 </template>
