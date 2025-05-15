b9c23c2dbbe296f7449a86298951a3f3f11e73a0
ShadFyt <doanryan10@gmail.com>
2025-05-15 16:53:47 -0400

    refactor: replace textrea with Textarea from shadcn vue

--- frontend/src/pages/journal-entries/new.vue
+++ frontend/src/pages/journal-entries/new.vue
@@ -59,7 +59,7 @@ const onSubmit = handleSubmit(async (values) => {
           <FormItem>
             <FormLabel>Content</FormLabel>
             <FormControl>
-              <textarea
+              <Textarea
                 v-bind="componentField"
                 class="flex min-h-24 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
                 placeholder="Describe your journal entry"
--- frontend/src/pages/projects/new.vue
+++ frontend/src/pages/projects/new.vue
@@ -69,11 +69,11 @@ const onSubmit = handleSubmit(async (values) => {
           <FormItem>
             <FormLabel>Description</FormLabel>
             <FormControl>
-              <textarea
+              <Textarea
                 v-bind="componentField"
                 class="flex min-h-24 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
                 placeholder="Describe your project"
-              ></textarea>
+              />
             </FormControl>
             <FormMessage />
           </FormItem>
